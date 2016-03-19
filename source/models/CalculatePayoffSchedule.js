e613d12d4aff264ab2ccb53d48258e9de1de6d61
Mike Palmer <mrpalmer685@gmail.com>
2016-03-19 15:29:21 -0400

    Added more functionality to CalculatePayoffSchedule

--- source/models/CalculatePayoffSchedule.js
+++ source/models/CalculatePayoffSchedule.js
@@ -1,14 +1,19 @@
 import reduce from 'lodash/reduce'
-import forEach from 'lodash/forEach'
 import map from 'lodash/map'
+import remove from 'lodash/remove'
 
 function balanceRemaining(accounts) {
     const accumulate = (total, account) => total + account.balance
     return reduce(accounts, accumulate, 0) > 0
 }
 
+function totalMonthlyPayment(accounts) {
+    const accumulate = (total, account) => total + Math.min(+account.minimumPayment, +account.balance)
+    return reduce(accounts, accumulate, 0)
+}
+
 function applyPayment(account, payment) {
-    const actualPayment = Math.min(account.balance, payment)
+    const actualPayment = Math.min(+account.balance, payment)
     account.balance -= actualPayment
     return actualPayment
 }
@@ -19,33 +24,48 @@ function accrueInterest(account) {
     return interestAccrued
 }
 
+function payAccount(account, budget) {
+    const paymentRecord = {
+        accountId: account.accountId,
+        startingBalance: account.balance
+    }
+
+    paymentRecord.payment = applyPayment(account, budget)
+    paymentRecord.interestAccrued = accrueInterest(account)
+    paymentRecord.endingBalance = account.balance
+    return paymentRecord
+}
+
 export default function (accounts, budget) {
-    const accountData = map(accounts, account => ({
+    let accountData = map(accounts, account => ({
         ...account,
         balance: parseFloat(account.balance),
         interestRate: parseFloat(account.interestRate),
         minimumPayment: parseFloat(account.minimumPayment)
     }))
 
-    const pay = account => {
-        const paymentRecord = {
-            accountId: account.accountId,
-            startingBalance: account.balance
-        }
+    const minimumBudget = totalMonthlyPayment(accountData)
+    if (minimumBudget > budget) {
+        throw new Error(
+            `Budget (${budget}) must be greater than or equal to the total monthly minimum payment (${minimumBudget}).`
+        )
+    }
 
-        paymentRecord.payment = applyPayment(account, budget)
-        paymentRecord.interestAccrued = accrueInterest(account)
-        paymentRecord.endingBalance = account.balance
-        return paymentRecord
+    function calculatePayment(account, index) {
+        const surplus = index === 0 ? budget - minimumBudget : 0
+        const budgetForAccount = account.minimumPayment + surplus
+        return payAccount(account, budgetForAccount)
     }
 
     const payments = []
     let month = 1
 
     while (balanceRemaining(accountData)) {
+        remove(accountData, account => account.balance <= 0)
+
         payments.push({
             month,
-            accounts: map(accountData, pay)
+            accounts: map(accountData, calculatePayment)
         })
         ++month
     }
--- test/models/CalculatePayoffSchedule.Test.js
+++ test/models/CalculatePayoffSchedule.Test.js
@@ -1,28 +1,8 @@
-import { expect } from 'chai'
+import chai from 'chai'
 import Account from 'models/Account'
 import calculate from 'models/CalculatePayoffSchedule'
 
-/*
-starting balance
-interest accrued
-payment
-ending balance
-
-[
-    {
-        month: <month>,
-        accounts: [
-            {
-                accountId,
-                startingBalance,
-                interestAccrued,
-                payment,
-                endingBalance
-            }
-        ]
-    }
-]
- */
+const expect = chai.expect
 
 describe('Calculate Payoff Schedule', () => {
     it('should return an empty array for no accounts', () => {
@@ -52,6 +32,11 @@ describe('Calculate Payoff Schedule', () => {
         ])
     })
 
+    it('should throw an error when the total monthly payment is greater than the budget', () => {
+        const account = new Account('test', '0', '100', '50')
+        expect(() => calculate([account], 10)).to.throw()
+    })
+
     it('should accrue interest on an account', () => {
         const account = new Account('test', '12', '100', '10')
         expect(calculate([account], 90)).to.eql([
@@ -78,5 +63,91 @@ describe('Calculate Payoff Schedule', () => {
         ])
     })
 
-    
+    it('should not return payment records for accounts with zero balance', () => {
+        const activeAccount = new Account('active', '12', '100', '10')
+        const inactiveAccount = new Account('inactive', '12', '0', '10')
+
+        expect(calculate([activeAccount, inactiveAccount], 100)).to.eql([
+            {
+                month: 1,
+                accounts: [{
+                    accountId: activeAccount.accountId,
+                    startingBalance: 100,
+                    payment: 100,
+                    interestAccrued: 0,
+                    endingBalance: 0
+                }]
+            }
+        ])
+    })
+
+    it('should only apply the minimum payment to all but the first account', () => {
+        const veryImportantAccount = new Account('very important', '12', '100', '10')
+        const lessImportantAccount = new Account('less important', '12', '100', '50')
+        expect(calculate([veryImportantAccount, lessImportantAccount], 150)).to.eql([
+            {
+                month: 1,
+                accounts: [
+                    {
+                        accountId: veryImportantAccount.accountId,
+                        startingBalance: 100,
+                        interestAccrued: 0,
+                        payment: 100,
+                        endingBalance: 0
+                    },
+                    {
+                        accountId: lessImportantAccount.accountId,
+                        startingBalance: 100,
+                        interestAccrued: 0.5,
+                        payment: 50,
+                        endingBalance: 50.5
+                    }
+                ]
+            },
+            {
+                month: 2,
+                accounts: [
+                    {
+                        accountId: lessImportantAccount.accountId,
+                        startingBalance: 50.5,
+                        payment: 50.5,
+                        interestAccrued: 0,
+                        endingBalance: 0
+                    }
+                ]
+            }
+        ])
+    })
+
+    it('should spend all available budget on the first account', () => {
+        const veryImportantAccount = new Account('very important', '12', '50', '10')
+        const lessImportantAccount = new Account('less important', '12', '100', '50')
+        const notImportantAccount = new Account('not important', '12', '10', '10')
+        expect(calculate([veryImportantAccount, lessImportantAccount, notImportantAccount], 100)[0]).to.eql({
+            month: 1,
+            accounts: [
+                {
+                    accountId: veryImportantAccount.accountId,
+                    startingBalance: 50,
+                    payment: 40,
+                    interestAccrued: 0.1,
+                    endingBalance: 10.1
+                },
+                {
+                    accountId: lessImportantAccount.accountId,
+                    startingBalance: 100,
+                    payment: 50,
+                    interestAccrued: 0.5,
+                    endingBalance: 50.5
+                },
+                {
+                    accountId: notImportantAccount.accountId,
+                    startingBalance: 10,
+                    payment: 10,
+                    interestAccrued: 0,
+                    endingBalance: 0
+                }
+            ]
+        })
+    })
 })
